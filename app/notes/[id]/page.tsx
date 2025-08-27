e1dbf37a3fe87aa324950b827d430cdb42632df1
Roman-Isko <roman_isko@ukr.net>
2025-08-28 00:33:30 +0300

    modified

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,3 +1,14 @@
+// import NoteDetailsWrapper from "./NoteDetailsWrapper";
+
+// interface NotePageProps {
+//   params: Promise<{ id: string }>;
+// }
+
+// export default async function NotePage({ params }: NotePageProps) {
+//   const { id } = await params;
+//   return <NoteDetailsWrapper noteId={id} />;
+// }
+
 import NoteDetailsWrapper from "./NoteDetailsWrapper";
 
 interface NotePageProps {
